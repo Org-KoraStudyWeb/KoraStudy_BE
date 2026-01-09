16895377c47dae716f268145e7799617bf97030b
VuMinhQuang <qunagminh32115@gmail.com>
2026-01-09 15:29:29 +0700

    fix bug

--- korastudy_be/src/main/java/korastudy/be/controller/PaymentController.java
+++ korastudy_be/src/main/java/korastudy/be/controller/PaymentController.java
@@ -88,7 +88,6 @@ public class PaymentController {
             }
 
             // Redirect về frontend với kết quả
-//            String redirectUrl = String.format("http://localhost3000/payment/result?paymentId=%d&status=%s", payment.getId(), status);
             String redirectUrl = String.format("https://witty-water-06494e100.6.azurestaticapps.net/payment/result?paymentId=%d&status=%s", payment.getId(), status);
             response.sendRedirect(redirectUrl);
 
